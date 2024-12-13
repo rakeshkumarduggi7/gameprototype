10fa5d9a87b850b067e8063fb667a09d8c98e62c
rakeshkumarduggi7 <rakeshfc3@gmail.com>
2024-12-14 02:13:56 +0530

    fup

--- core/src/com/gameproto/game/breakintoact3.java
+++ core/src/com/gameproto/game/breakintoact3.java
@@ -26,18 +26,24 @@ public class breakintoact3 implements Screen {
     OrthographicCamera c;
     Viewport vp;
     SpriteBatch batch;
-    String s; String x;
+    StringBuilder s; StringBuilder x;
     Rectangle o1;
     Rectangle o2;
     Rectangle o3;
     Rectangle p;
     ArrayList<Rectangle> ar;
     ArrayList<String> as;
-    boolean b=false;
+    int ref=8;
+    boolean b=false; Vector3 tp=new Vector3();
     HashMap<Rectangle,String> hm;
+    ArrayList<String> sa=new ArrayList<>();
+    boolean a=false;
+    ArrayList<Rectangle> cr;
+
+    ArrayList<String> cs;
     public breakintoact3(gameproto gp){
         this.gp=gp;
-        x="louis: how can i get out of here? If i cant not only me ben too will be struck here forever.\n" +
+        x=new StringBuilder("louis: how can i get out of here? If i cant not only me ben too will be struck here forever.\n" +
                 "as louis is thinking to himself ,a soldier comes in a hurried manner  to another soldier in a frightened  way \n" +
                 "soldier 1: the  -Apophis- is cursing us!!! we are surrounded.!!!!\n" +
                 "soldier 2: what happened !!!\n" +
@@ -46,7 +52,7 @@ public class breakintoact3 implements Screen {
                 "meanwhile....\n" +
                 "ben : this is my only shot it should scare them pretty much....\n" +
                 "(gp : ben choses amap ways he can scare by using the magic power of pendant and his knowledge of how)\n" +
-                "so he created a ruckus of pretending like a god and using pendant to travel in different directions and shout so the soldiers believe that he is omni present god,, a few soldiers attention was there at the beginning and later all the soldiers in the prison building were frightened and when ben got all of their attention he demanded that he release prof. and the pendant that was stolen from him. ";
+                "so he created a ruckus of pretending like a god and using pendant to travel in different directions and shout so the soldiers believe that he is omni present god,, a few soldiers attention was there at the beginning and later all the soldiers in the prison building were frightened and when ben got all of their attention he demanded that he release prof. and the pendant that was stolen from him. ");
           batch=new SpriteBatch(); i=0;
         j=0;
         count=0;   FreeTypeFontGenerator ftf=new FreeTypeFontGenerator(Gdx.files.internal("gamefont.ttf"));
@@ -70,6 +76,17 @@ public class breakintoact3 implements Screen {
         as.add("go left ");
         as.add("go right ");
         as.add("go behind ");
+sa.add("\nben goes to the left of the prison \n");
+sa.add("\nben goes to the right of the prison \n");
+sa.add("\nben goes to the behind the prison \n");
+cr=new ArrayList<>();
+Rectangle c1=new Rectangle();
+        Rectangle c2=new Rectangle();
+        Rectangle c3=new Rectangle();
+        cs=new ArrayList<>();
+
+
+
     }
     @Override
     public void show() {
@@ -77,11 +94,11 @@ public class breakintoact3 implements Screen {
     }
     @Override
     public void render(float delta) {
-        s=x.substring(j,i);
+        s=new StringBuilder(x.substring(j,i));
         ScreenUtils.clear(1,255f/255f,240f/255f,0);
-        if(count==8){
+        if(count==ref){
             p=new Rectangle(500,100,100,72);
-            Vector3 tp=new Vector3();
+
             tp.set(Gdx.input.getX(), Gdx.input.getY(), 0);
             c.unproject(tp);
             if (Gdx.input.isTouched()&&p.contains(tp.x,tp.y)){
@@ -98,14 +115,30 @@ b=true;
           for (Rectangle r:ar){
               r=new Rectangle(r.x,r.y,r.width,r.height);
               bmf.draw(batch,as.get(s),r.x,r.y+72);
-              s++;
-              if(s>=as.size()){
-                  s=0;
+              if (Gdx.input.isTouched()){
+                  tp.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+                  c.unproject(tp);
+                  if (r.contains(tp.x,tp.y)){
+                    ar.remove(ar.size()-1);
+                    as.remove(s);
+                    b=false;
+                     x.insert(i,sa.get(s));
+                    sa.remove(s);
+                    ref++;
+                      a=true;
+                    break;
+                  }
               }
+              if (as.size()==0)break;
+              s++;
+
           }
       }
+        if (a){
+
+        }
         batch.end();
-        if (count!=8){
+        if (count!=ref){
         if (i<x.length()-2&&x.charAt(i+1)=='\n'){
             if ((Gdx.input.isKeyPressed(Input.Keys.ENTER)||Gdx.input.isTouched() )){
                 i++;
